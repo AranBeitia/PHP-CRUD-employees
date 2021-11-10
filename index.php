8973cd4707b247e55c081be4df2c0f04973b62e4
AranBeitia <arantza.beitia@gmail.com>
2021-11-10 16:08:39 +0100

    modules

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <!-- TODO Application entry point. Login view -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Employees - login</title>
-</head>
+<?php include './src/module/head.php' ?>
 <body>
 	<h1>login</h1>
 	<a href="./src/dashboard.php">Go to dashboard</a>
--- src/dashboard.php
+++ src/dashboard.php
@@ -1,12 +1,5 @@
 <!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Dashboard</title>
-</head>
+<?php include './src/module/head.php' ?>
 <body>
 	<h1>dashboard</h1>
 	<a href="./employee.php">Go to employee</a>
