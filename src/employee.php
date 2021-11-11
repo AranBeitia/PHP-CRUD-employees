7d5620a1a7b0c21b4d6bd082b11b3c223ce299e2
AranBeitia <arantza.beitia@gmail.com>
2021-11-11 09:57:51 +0100

    data table

--- src/employee.php
+++ src/employee.php
@@ -1,13 +1,5 @@
 <!-- TODO Employee view -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Employees</title>
-</head>
-
+<?php include './module/head.php' ?>
 <body>
 	<h1>employee</h1>
 	<a href="./dashboard.php">Go back</a>
--- src/module/head.php
+++ src/module/head.php
@@ -6,23 +6,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Employees</title>
 
-	<link rel="stylesheet" type="text/css" href="../../css/demos.css" />
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600,400' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600,400' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="../css/main.css" />
 
-    <link rel="stylesheet" type="text/css" href="../../css/jsgrid.css" />
-    <link rel="stylesheet" type="text/css" href="../../css/theme.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
+	<link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css" /> 
+	<link type="text/css" rel="stylesheet"    href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css" /> 
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js"></script>
 
-    <script src="../external/jquery/jquery-1.8.3.js"></script>
-    <script src="db.js"></script>
-
-    <script src="../src/jsgrid.core.js"></script>
-    <script src="../src/jsgrid.load-indicator.js"></script>
-    <script src="../src/jsgrid.load-strategies.js"></script>
-    <script src="../src/jsgrid.sort-strategies.js"></script>
-    <script src="../src/jsgrid.field.js"></script>
-    <script src="../src/fields/jsgrid.field.text.js"></script>
-    <script src="../src/fields/jsgrid.field.number.js"></script>
-    <script src="../src/fields/jsgrid.field.select.js"></script>
-    <script src="../src/fields/jsgrid.field.checkbox.js"></script>
-    <script src="../src/fields/jsgrid.field.control.js"></script>
+	<script src="../js/jsGrid.js" defer></script>
 </head>
