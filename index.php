869c23a740e7205df17a54981f4e9e0428d7cc72
Josep Romero Ros <joseprrr@gmail.com>
2021-11-11 10:25:50 +0100

    working on login

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <!-- TODO Application entry point. Login view -->
 <?php include './src/module/head.php' ?>
 <body>
-	<h1>login</h1>
-	<a href="./src/dashboard.php">Go to dashboard</a>
+	<?php require_once("./assets/header.html"); ?>
+	<main class="container align-middle">
+       <div class="container">
+            <h3 class="mb-4">Please Log in</h3>
+            <form action="./src/library/loginController.php" method="POST" class="form">
+                <div>
+                    <input name="email" type="email" id="email" placeholder="Email"></input>
+                </div>
+                <div>
+                    <input name="pass" type="password" id="password" placeholder="Password">
+                </div>
+                <div>
+                    <button type="submit" class="btn-3d">Submit</button>
+                </div>
+                <?= $php ? "<div class='alert__msg alert-$php[type] role='alert'>$php[text]</div>" : "" ?>
+            </form>
+        </div>
+    </main>
+    <div>
+    </div>
+    <?php require_once("./assets/footer.html"); ?>
 </body>
 </html>
---
+++ src/library/loginController.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("./loginManager.php");
+    
+    if(isset($_POST["email"] && $_POST["pass"])){
+        $email = $_POST["email"];
+        $pass = $_POST["pass"];
+    }
+    
+
+    
+
+    
